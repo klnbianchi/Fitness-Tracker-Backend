79a4bbec23844de66ec73cb0953b8b6cf5404d96
Luke <reboluketion42@gmail.com>
2021-10-18 20:53:32 -0700

    server is up

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // create the express server here
+require('dotenv').config();
+
+const express = require('express');
+const {PORT = 3000} = process.env;
+const server = express();
+
+server.use(express.json());
+
+const morgan = require("morgan");
+server.use(morgan("dev"));
+
+const cors = require("cors")
+server.use(cors());
+
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
+
+const {client} = require("./db/index.js");
+
+server.use(function (req,res,next){
+    res.status(404).send('Unable to find the requested resource!')
+})
+
+server.use(function (err,req,res,next){
+    res.status(500).send({error: err.message})
+})
+
+
+server.listen(PORT, ()=>{
+
+    console.log("The server is up on port", PORT);
+})
+
+client.connect();
