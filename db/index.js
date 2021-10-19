d5e7262854cf54a2cddeaae2e5b9a584177617ac
Kendra <klnbianchi@gmail.com>
2021-10-18 20:54:50 -0700

    worked on client and index in db

--- db/index.js
+++ db/index.js
@@ -1,1 +1,10 @@
 // require and re-export all files in this db directory (users, activities...)
+const { client } = require('./client');
+const { rebuildDB } = require('./seedData')
+
+
+
+module.exports={
+    client,
+    rebuildDB
+}
