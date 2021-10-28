030f15d3c75bfa321fc9a1c97a12e66c2d5e64a7
Kendra <klnbianchi@gmail.com>
2021-10-27 18:17:18 -0700

    fixed login issue

--- api/index.js
+++ api/index.js
@@ -11,32 +11,32 @@ const usersRouter = require('./users');
 const activitesRouter= require('./activities');
 const activitiesRouter = require("./activities");
 
-apiRouter.use(async (req, res, next) => {
-    const prefix = 'Bearer ';
-    const auth = req.header('Authorization');
+// apiRouter.use(async (req, res, next) => {
+//     const prefix = 'Bearer ';
+//     const auth = req.header('Authorization');
   
-    if (!auth) {
-      next();
-    } else if (auth.startsWith(prefix)) {
-      const token = auth.slice(prefix.length);
+//     if (!auth) {
+//       next();
+//     } else if (auth.startsWith(prefix)) {
+//       const token = auth.slice(prefix.length);
   
-      try {
-        const { id } = jwt.verify(token, JWT_SECRET);
+//       try {
+//         const { id } = jwt.verify(token, JWT_SECRET);
   
-        if (id) {
-          req.user = await getUserById(id);
-          next();
-        }
-      } catch ({ name, message }) {
-        next({ name, message });
-      }
-    } else {
-      next({
-        name: 'AuthorizationHeaderError',
-        message: `Authorization token must start with ${prefix}`
-      });
-    }
-  });
+//         if (id) {
+//           req.user = await getUserById(id);
+//           next();
+//         }
+//       } catch ({ name, message }) {
+//         next({ name, message });
+//       }
+//     } else {
+//       next({
+//         name: 'AuthorizationHeaderError',
+//         message: `Authorization token must start with ${prefix}`
+//       });
+//     }
+//   });
 
   apiRouter.use('/users', usersRouter);
 apiRouter.use('/activities', activitiesRouter);
