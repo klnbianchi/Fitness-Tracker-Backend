c86edbd6c0540f9b75fc75b4b8e844af9a091b4a
Kendra <klnbianchi@gmail.com>
2021-10-28 12:29:20 -0700

    Co-authored-by: lukejosephbishop <lukejosephbishop@users.noreply.github.com>

--- api/activities.js
+++ api/activities.js
@@ -2,11 +2,7 @@ const express = require("express");
 const activitiesRouter = express.Router();
 const{
 getAllActivities,
-createActivity,
-updateActivity,
-getPublicRoutinesByActivity,
-}= require('../db');
-const { requireUser } = require("./utils");
+}= require('../db')
 
 activitiesRouter.get('/', async (req, res, next)=>{
 try{
@@ -20,39 +16,4 @@ try{
 }
 });
 
-activitiesRouter.post('/', async (req,res,next)=>{
-    const {name, description} = req.body
-try{
-    const newActivity = await createActivity({name, description});
-    res.send(newActivity)
-}catch(error){
-    console.error(error);
-    next(error);
-}
-});
-
-activitiesRouter.patch('/:activityId', requireUser, async (req,res, next)=>{
-const {activityId:id} = req.params;
-const {name, description } = req.body;
-
-try{
-const updatedActivity = await updateActivity({ id, name, description });
-res.send(updatedActivity);
-
-}catch(error){
-console.error(error);
-next(error)
-}
-});
-
-activitiesRouter.get('/:activityId/routines', async (req, res, next)=>{
-const {activityId: id} = req.params;
-try{
-const routines = await getPublicRoutinesByActivity({id});
-res.send(routines);
-}catch(error){
-    console.error(error);
-    next(error);
-}
-})
 module.exports = activitiesRouter;
